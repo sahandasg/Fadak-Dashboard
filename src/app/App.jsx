eed7d7f954a11eb42cfd44e797eeda9b10830034
sahand <sahandasg56@gmail.com>
2024-10-24 09:53:00 +0330

    create ThemeContext.js and develop custom ThemeSwitcher.jsx #6

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,19 @@
-import * as React from 'react';
+import {useState} from "react";
+import ThemeContext from "./contexts/ThemeContext";
 import Header from "./components/Header/Header";
 
 function App() {
+    if (!localStorage.getItem("theme")) {
+        localStorage.setItem("theme", "light")
+    }
+    const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
     return (
+        <ThemeContext.Provider value={{theme, setTheme,}}>
+            <div className={`${theme === "light" ? "light" : "dark"}`}>
                 <Header/>
+            </div>
+        </ThemeContext.Provider>
     );
 }
 
--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
+import ThemeSwitcher from "./ThemeSwitcher";
+import {useContext} from "react";
+import ThemeContext from "../../contexts/ThemeContext";
+
 
 function Header(props) {
+    const contextData = useContext(ThemeContext);
 
     return (
         <div
             id="header"
             className="flex gap-4 fixed top-0 w-full h-16 bg-main border-b-[1.2px] border-divider items-center justify-between px-4">
             <p className="font-semibold text-xl text-secondary hidden md:flex ">Fadak-Dashboard</p>
+            <div
+                className="flex md:hidden justify-center items-center p-1 rounded cursor-pointer transition-all hover:bg-third"
+                onClick={() => {
+                    contextData.setIsShow(contextData.isShow === "true" ? "false" : "true")
+                    localStorage.setItem("showSecondaryMenu", contextData.isShow === "true" ? "false" : "true")
+                }}
+            >
+            </div>
+            <ThemeSwitcher/>
         </div>
     )
         ;
