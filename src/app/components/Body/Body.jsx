11e5a2ba35da6eef6b6f3f49fb2fee271fa6bb84
sahand <sahandasg56@gmail.com>
2024-10-27 10:00:38 +0330

    develop Body.jsx #9

---
+++ src/app/components/Body/Body.jsx
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import ThemeContext from "../../contexts/ThemeContext";
+import {useRoutes} from "react-router-dom";
+import routes from "../../routes";
+
+function Body(props) {
+    const contextData = useContext(ThemeContext);
+    const router = useRoutes(routes)
+
+    return (
+        <div id="body"
+            className={`absolute transition-all min-h-[calc(100vh-4rem)] top-16 right-0 overflow-auto bg-bg ${contextData.isShow === "true" ? "md:w-[calc(100vw-15rem)] w-full" : "md:w-[calc(100vw-4rem)] w-full"}`}>
+            <div
+                className="transition-all w-full flex justify-center">
+                {router}
+            </div>
+        </div>
+    );
+}
+
+export default Body;
