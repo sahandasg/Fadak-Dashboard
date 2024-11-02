2862ca3712abea0737f5cb46f1b1099b85a818fc
sahand <sahandasg56@gmail.com>
2024-11-02 09:39:24 +0330

    develop custom data table with pagination #12

---
+++ src/app/components/DataTable/DataTable.jsx
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import {useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
+//css
+import "./dataTable.css"
+
+//data
+import {orders} from '../../../fake-db/db';
+
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const {count, page, rowsPerPage, onPageChange} = props;
+
+    const handleFirstPageButtonClick = (event) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{flexShrink: 0, ml: 2.5}}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label="previous page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
+            </IconButton>
+        </Box>
+    );
+}
+
+const rows = orders
+export default function CustomPaginationActionsTable() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    return (
+        <TableContainer component={Paper} sx={{maxHeight:"60vh"}}>
+            <Table sx={{minWidth: 500, backgroundColor: "var(--input-bg)",}} aria-label="custom pagination table">
+                <TableBody>
+                    {(rowsPerPage > 0
+                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rows
+                    ).map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell sx={{color: "var(--secondary)"}} component="th" scope="row">
+                                {row.id}
+                            </TableCell>
+                            <TableCell style={{width: 260, color: "var(--secondary)"}} align="left">
+                                {row.name}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.email}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.phoneNumber}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.status}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.amount}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.date}
+                            </TableCell>
+                            <TableCell style={{width: 160, color: "var(--secondary)"}} align="left">
+                                {row.time}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                    {emptyRows > 0 && (
+                        <TableRow style={{height: 53 * emptyRows}}>
+                            <TableCell colSpan={8}/>
+                        </TableRow>
+                    )}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            sx={{color:"var(--secondary)"}}
+                            rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+                            colSpan={8}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            slotProps={{
+                                select: {
+                                    inputProps: {
+                                        'aria-label': 'rows per page',
+                                    },
+                                    native: true,
+                                },
+                            }}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    );
+}
