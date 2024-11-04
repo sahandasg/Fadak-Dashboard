9a47babc2761af3dac1d4abcdcebb5546ba12e08
sahand <sahandasg56@gmail.com>
2024-11-04 09:00:47 +0330

    create fake db and add data

---
+++ src/fake-db/db/index.js
@@ -0,0 +1,122 @@
+export const orders = [
+    {
+        id: 1,
+        name: 'Sahand Asgharzadeh',
+        email: 'sahandasg56@gmail.com',
+        phoneNumber: '09146903072',
+        status: 'done',
+        amount: '15,000,000',
+        date: "1403/07/21",
+        time: "11:07"
+    },
+    {
+        id: 2,
+        name: 'Milad Abdollahi',
+        email: 'miladabdolahi@gmail.com',
+        phoneNumber: '09145334323',
+        status: 'pending',
+        amount: '32,000,000',
+        date: "1403/07/21",
+        time: "09:37"
+    },
+    {
+        id: 3,
+        name: 'Sina Moradi',
+        email: 'sinamoradi@gmail.com',
+        phoneNumber: '09148796543',
+        status: 'canceled',
+        amount: '2,000,000',
+        date: "1403/07/21",
+        time: "14:17"
+    },
+    {
+        id: 4,
+        name: 'Mohammad Mashayekhi',
+        email: 'mohammadmash@gmail.com',
+        phoneNumber: '09358123421',
+        status: 'done',
+        amount: '21,000,000',
+        date: "1403/07/20",
+        time: "11:42"
+    },
+    {
+        id: 5,
+        name: 'Amirhossein khazayi',
+        email: 'amirhosseinkhazayi@gmail.com',
+        phoneNumber: '09219874345',
+        status: 'canceled',
+        amount: '16,720,000',
+        date: "1403/07/20",
+        time: "16:56"
+    },
+    {
+        id: 6,
+        name: 'Amirhossein Ramazani',
+        email: 'amirhosseinramazani@gmail.com',
+        phoneNumber: '09195443242',
+        status: 'pending',
+        amount: '6,450,000',
+        date: "1403/07/20",
+        time: "21:06"
+    },
+    {
+        id: 7,
+        name: 'Hamed Abdollahi',
+        email: 'hamedabdollahi@gmail.com',
+        phoneNumber: '09122234354',
+        status: 'pending',
+        amount: '3,310,000',
+        date: "1403/07/25",
+        time: "21:26"
+    },
+    {
+        id: 8,
+        name: 'Hamed Norozi',
+        email: 'hamednorozi@gmail.com',
+        phoneNumber: '09919873243',
+        status: 'done',
+        amount: '12,365,000',
+        date: "1403/07/25",
+        time: "19:51"
+    },
+    {
+        id: 9,
+        name: 'Amir Garmarodi',
+        email: 'amirgarmarodi@gmail.com',
+        phoneNumber: '09143235987',
+        status: 'canceled',
+        amount: '8,700,000',
+        date: "1403/07/25",
+        time: "17:31"
+    },
+    {
+        id: 10,
+        name: 'Amiralli Asgarzadeh',
+        email: 'amiralliasgarzadeh@gmail.com',
+        phoneNumber: '09919143234',
+        status: 'pending',
+        amount: '1,200,000',
+        date: "1403/07/25",
+        time: "12:21"
+    },
+    {
+        id: 11,
+        name: 'Mohssen Hemati',
+        email: 'mohssenhemati@gmail.com',
+        phoneNumber: '09144237031',
+        status: 'pending',
+        amount: '11,280,000',
+        date: "1403/07/25",
+        time: "22:17"
+    },
+]
+
+export const sales = [
+    {
+        label: 'Total Sales ($)',
+        data: [
+            3423, 2210, 764, 1879, 1478, 1373, 1891, 2171, 620, 1269, 724, 1707, 1188,
+            1879, 626, 1635, 2177, 516, 1793, 1598,
+        ],
+    },
+]
